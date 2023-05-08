60cd0f30084877fc9e666d16ae31a3e272defd1c
joyce lau <jshizuki@yahoo.com.hk>
2023-05-08 12:52:23 +0900

    Second Challenge

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # verb "url", to: "controller#action"
+  root to: 'questions#ask'
+  get '/answer', to: 'questions#answer', as: :answer
 end
